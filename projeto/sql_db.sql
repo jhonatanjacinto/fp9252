6693f9a8efdb67c3978c61b419071390dce88e34
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2020-08-28 21:51:53 -0300

    SQL Tabelas Adicionais

--- projeto/sql_db.sql
+++ projeto/sql_db.sql
@@ -20,3 +20,34 @@ CREATE TABLE contatos (
     mensagem TEXT NOT NULL,
     data_contato TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE depoimentos (
+    depoimento_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    texto VARCHAR(150) NOT NULL,
+    foto VARCHAR(50) DEFAULT NULL,
+    ativo BOOLEAN DEFAULT 1
+);
+
+CREATE TABLE nosso_time (
+    membro_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    cargo VARCHAR(50) NOT NULL,
+    minicurriculo VARCHAR(150) NOT NULL,
+    foto VARCHAR(50) DEFAULT NULL,
+    ativo BOOLEAN DEFAULT 1
+);
+
+CREATE TABLE usuarios_admin (
+    usuario_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    email_login VARCHAR(50) NOT NULL,
+    senha VARCHAR(50) NOT NULL,
+    ativo BOOLEAN DEFAULT 1
+);
+
+CREATE TABLE servicos (
+    servico_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    nome_servico VARCHAR(50) NOT NULL,
+    texto_servico TEXT NOT NULL,
+    ativo BOOLEAN DEFAULT 1
+);
